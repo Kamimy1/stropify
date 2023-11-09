f0beeb580eb691559a38857610701ee83ced6569
Africa Maria Bermudez Mejias <africabermudez2003@gmail.com>
2023-11-09 15:08:46 +0100

    9/11/2023

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit{
+
+  mainMenu: {
+    defaultOptions: Array<any>, accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] }
+
+  customOptions: Array<any> = []
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-home',
+        router: ['/', 'auth']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search-alt',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-books',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' }
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil uil-plus'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil uil-heart'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'PORTALS (Deluxe)',
+        router: ['/']
+      },
+      {
+        name: 'Mix reggaeton',
+        router: ['/']
+      },
+      {
+        name: 'phonk',
+        router: ['/']
+      },
+      {
+        name: 'Pipo',
+        router: ['/']
+      }
+    ]
+      
+  }
 
 }
